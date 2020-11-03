6f5372b392952559a264a434aa85a28ad70f44dd
Thu Nghiem <richardsnyderyp@gmail.com>
2020-11-03 23:49:33 +0200

    change link order

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
-import { Brightness6Rounded } from "@material-ui/icons";
 import Head from "next/head";
 import Link from "next/Link";
 import { useEffect, useState } from "react";
+import { Brightness6Rounded } from "@material-ui/icons";
 import styles from "./Layout.module.css";
 
 const Layout = ({ children, title = "World Ranks" }) => {
